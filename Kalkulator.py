18a4098813349b68416a0c433dfb537fe78c071a
Agnieszka Nowotna <a.ilkow7@gmail.com>
2023-01-28 10:37:17 +0100

    Change definition

--- Kalkulator.py
+++ Kalkulator.py
@@ -4,49 +4,27 @@ from enum import IntEnum
 
 def check_if_float(number):
     try:
-        float(number)
+        number = float(number)
         return True
     except ValueError:
         return False
 
-def check_if_number1():
-    j = 1
-    while j > 0:
-        number = input("Wprowadź liczbę:")
-        if check_if_float(number) == True:
-            j -= 1
+def get_numbers():
+    array_of_numbers = []
+    i = 0
+    while True:
+        if i ==0:
+            text = "Wprowadź pierwszą liczbę: "
         else:
-            print("Wprowadzona wartość nie jest liczbą")
-            print("Proszę o ponowne wprowadzenie wartości")
+            text = "Wprowadź kolejną liczbę lub 'stop' aby uzyskać wynik działania: "
+        i += 1
+        number = input(text)
+        if number == "stop":
+            return array_of_numbers
+        if not check_if_float(number):
+            print("Wprowadzona wartość nie jest liczbą") 
             continue
-    return float(number)
-
-def check_if_number2():
-    j = 1
-    while j > 0:
-        number = input('Wprowadź kolejną liczbę lub wpisz "stop" aby uzyskać rezultat działania:')
-        if check_if_float(number) == True or number == "stop" or number == "Stop":
-            j -= 1
-        else:
-            print("Wprowadzona wartość nie jest liczbą")
-            print("Proszę o ponowne wprowadzenie wartości")
-            continue
-    return number
-
-def check_if_number3():
-    i = 1
-    while i > 0:
-        number = (input("Wprowadź liczbę: "))
-        if check_if_float(number) == True:
-            pass
-        else:
-            print("Wprowadzona wartość nie jest liczbą")
-            print("Proszę o ponowne wprowadzenie wartości")
-            continue
-        i -=1
-    i = 1
-    return float(number)
-
+        array_of_numbers.append(float(number))
 
 Operation = IntEnum('Operation','adding subtraction multiplication dividing')
 operation_type = int(input("""Podaj działanie, wprowadzająć odpowiednią liczbę: 
@@ -57,66 +35,39 @@ operation_type = int(input("""Podaj działanie, wprowadzająć odpowiednią licz
 """))
 
 if operation_type == Operation.adding:
-    numbers = []
-    i = 1
-    while i > 0:
-        if i < 3:
-            x = check_if_number1()
-            numbers.append(x)
-            i += 1
-        else:
-            x = check_if_number2()
-            if x == "stop" or x == "Stop":
-                i -= i
-            else:
-                numbers.append(float(x))
-    numbers_string = ', '.join([str(number) for number in numbers])
+    array_of_numbers = get_numbers()
+    numbers_string = ', '.join([str(number) for number in array_of_numbers])
     logging.info(f'Dodaję liczby: {str(numbers_string)}')
-    result = 0
-    for number in numbers:
-        result += number
+    result = sum(array_of_numbers)
     print(f'Wynik to: {result}')
 
 if operation_type == Operation.subtraction:
-    x = check_if_number3()
-    y = check_if_number3()
-    logging.info (f'Odejmuję liczbę {y} od liczby {x}')
-    print (f'Wynik to: {x-y}')
+    array_of_numbers = get_numbers()
+    numbers_string = ', '.join([str(number) for number in array_of_numbers[1:]])
+    logging.info (f'Od liczby {str(array_of_numbers[0])} odejmuję liczby {numbers_string}')
+    print (f'Wynik to: {array_of_numbers[0] - sum(array_of_numbers[1:])}')
 
 if operation_type == Operation.multiplication:
-    numbers = []
-    i = 1
-    while i > 0:
-        if i < 3:
-            x = check_if_number1()
-            numbers.append(x)
-            i += 1
-        else:
-            x = check_if_number2()
-            if x == "stop" or x == "Stop":
-                i -= i
-            else:
-                numbers.append(float(x))
-    numbers_string = ', '.join([str(number) for number in numbers])
+    array_of_numbers = get_numbers()
+    numbers_string = ', '.join([str(number) for number in array_of_numbers])
     logging.info(f'Mnożę liczby: {str(numbers_string)}')
     product = 1
-    for number in numbers:
+    for number in array_of_numbers:
         product *= number
     print(f'Wynik to: {product}')
 
 if operation_type == Operation.dividing:
-    x = check_if_number3()
-    y = check_if_number3()
-    i = 1
-    while i > 0:
-        if y == 0.0:
+    array_of_numbers = get_numbers()
+    for number in array_of_numbers[1:]:
+        if number == 0.0:
             logging.info('Nie można dzielić przez zero, wprowadź inną wartość')
-            y = check_if_number3()
-            continue
-        else:
-            i -= 1
-    logging.info (f'Dzielę liczbę {x} przez liczbę {y}')
-    print (f'Wynik to: {x/y}')
+            array_of_numbers[array_of_numbers.index(0)] = float(input("Wprowadź nową wartość:"))
+    numbers_string = ', '.join([str(number) for number in array_of_numbers[1:]])
+    logging.info (f'Dzielę liczbę {str(array_of_numbers[0])} przez liczby {numbers_string}')
+    result = array_of_numbers[0]
+    for number in array_of_numbers[1:]:
+        result = result/number
+    print (f'Wynik to: {round(result,3)}')
 
 if operation_type > 4:
     print("Wprowadzono nieprawidłową wartość")
